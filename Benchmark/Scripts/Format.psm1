38ae5eb04aa10ac2d29f8c346fb25613bffb5943
Kiko <kiko@autarky.digital>
2026-08-12 18:50:28 +0200

    trying to fix benchmarking comparison by deleting the multiple runtimes bench which requires the runtimes to be installed seperately

--- Benchmark/Scripts/Format.psm1
+++ Benchmark/Scripts/Format.psm1
@@ -5,15 +5,15 @@ function Format-Value {
     param (
         [Parameter(Mandatory = $true, Position = 0)]
         [double]$Value,
-
+        
         [Parameter(Mandatory = $true, Position = 1)]
         [string]$Unit
     )
     
     $unitType = Get-Unit-Type -Unit $Unit
     switch -CaseSensitive ($unitType) {
-        "duration"  { return "$("{0:N2}" -f $Value) $Unit" }
-        "memory"    { return "$("{0:N2}" -f $Value) $Unit" }
+        "duration"  { return "$("{0:N2}" -f $Value)$Unit" }
+        "memory"    { return "$("{0:N2}" -f $Value)$Unit" }
         "count"     { return "$("{0:N0}" -f $Value)" }
         default  {
             Write-Error "Formatting failed due to unknow unit type '$unitType'."
